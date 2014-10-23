602e1ab0c3ecf90e90404cc12510eda2c4c7e40e
Andy Simpson <andy@aiusepsi.co.uk>
2014-10-24 00:55:48 +0100

    Fall back to make if ninja not available.

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 mkdir build
 cd build
-cmake .. -GNinja
-ninja
+
+# Detect ninja
+if command -v ninja &>/dev/null; then
+	export BUILD_CMD='ninja'
+	export CMAKE_ARGS='-GNinja'	
+else
+	export BUILD_CMD='make'
+	export CMAKE_ARGS=''
+fi
+
+cmake $CMAKE_ARGS ..
+$BUILD_CMD
