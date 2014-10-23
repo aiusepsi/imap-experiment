1bc944b836a0e469565369b9a18261769638552c
Andy Simpson <andy@aiusepsi.co.uk>
2014-10-24 00:05:55 +0100

    Made sure C++11 support worked

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <boost/asio.hpp>
+#include <iostream>
+//#include <functional>
+
+using namespace std;
 
 int main()
 {
+	auto f = [](string s){ cout << s << endl; };
+	f("Hello, world!");
 	return 0;
 }
